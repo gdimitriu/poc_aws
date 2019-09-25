9bdd15ba66b10e3b52f124f821540805123c692d
Gabriel Dimitriu <gabriel.dimitriu@yahoo.com>
2019-09-25 13:50:45 +0300

    addded copyright to ec2instances

--- src/main/java/poc_aws/ec2/EC2Instances.java
+++ src/main/java/poc_aws/ec2/EC2Instances.java
@@ -1,3 +1,22 @@
+/*
+ Copyright (c) 2019 Gabriel Dimitriu All rights reserved.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+
+ This file is part of poc_aws project.
+
+ poc_aws is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ poc_aws is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with poc_aws.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package poc_aws.ec2;
 
 import com.amazonaws.services.ec2.AmazonEC2;
