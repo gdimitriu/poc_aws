dc0d8b9f3746c7c8821cb1644b55555a705a19ee
Gabriel Dimitriu <gabriel.dimitriu@yahoo.com>
2019-12-06 11:12:18 +0200

    added install scripts and json for could formation

---
+++ src/main/resources/install_scripts/pharmaweb2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+exec > /tmp/start.log  2>&1
+sudo yum update -y
+sudo yum install httpd -y
+sudo chkconfig httpd on
+sudo /etc/init.d/httpd start
+aws s3 cp s3://gabrieldimitriupharmaweb/pharma_webservers/web2.zip web.zip
+sudo unzip web.zip -d /var/www/html/
+rm web.zip
+aws s3 cp s3://gabrieldimitriupharmaweb/pharma_webservers/welcome.conf welcome.conf
+sudo cp welcome.conf /etc/httpd/conf.d/
+sudo /etc/init.d/httpd restart
